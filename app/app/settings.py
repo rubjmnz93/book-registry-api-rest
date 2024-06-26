55540bdcd737fea275d943e7c881003406bac470
Ruben Jimenez Andreu <rubenjmnz93@gmail.com>
2024-06-26 09:28:48 +0200

    Added default values for enviroment variables.

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="b5ede651f055275769e5146d")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", bool)
+DEBUG = config("DEBUG", cast=bool, default=True)
 
 ALLOWED_HOSTS = []
 
